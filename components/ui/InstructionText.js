b2c35c11ad4b1825ba9af10860544fdd97a9c62f
Ananthu S <501618@containerstore.com>
2023-01-01 20:55:41 +0530

    68 - cascading styles

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -2,8 +2,9 @@ import React from "react";
 import { StyleSheet, Text } from "react-native";
 import Colors from "../../constants/colors";
 
-function InstructionText({ children }) {
-	return <Text style={styles.instructionText}>{children}</Text>;
+function InstructionText({ children, style }) {
+	//style are evaulated from left to right. The right items can override the previous styles.
+	return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -59,14 +59,20 @@ function GameScreen({ enteredNumber: userNumber, gotoNextStage }) {
 			<Title style={styles.title}>Opponent's Guess</Title>
 			<NumberContainer>{currentGuess}</NumberContainer>
 			<Card>
-				<InstructionText>Higher or lower?</InstructionText>
-				<View>
-					<PrimaryButton onPress={nextGuessHandler.bind(null, "lower")}>
-						-
-					</PrimaryButton>
-					<PrimaryButton onPress={nextGuessHandler.bind(null, "greater")}>
-						+
-					</PrimaryButton>
+				<InstructionText style={styles.instructionText}>
+					Higher or lower?
+				</InstructionText>
+				<View style={styles.buttonsContainer}>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={nextGuessHandler.bind(null, "lower")}>
+							-
+						</PrimaryButton>
+					</View>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={nextGuessHandler.bind(null, "greater")}>
+							+
+						</PrimaryButton>
+					</View>
 				</View>
 			</Card>
 		</View>
@@ -77,6 +83,15 @@ const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
 		padding: 24,
+	},
+	buttonsContainer: {
+		flexDirection: "row",
+	},
+	buttonContainer: {
+		flex: 1,
+	},
+	instructionText: {
+		marginBottom: 12,
 	},
 });
 
