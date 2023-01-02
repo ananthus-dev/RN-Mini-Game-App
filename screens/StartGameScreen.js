4fb5239785bf817137d983f68e20d497a615006e
Ananthu S <501618@containerstore.com>
2023-01-03 01:39:02 +0530

    83,84,85-dynamic sizes, keyboard avoding view

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,15 @@
 import { useState } from "react";
-import { Alert, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+	Alert,
+	Dimensions,
+	KeyboardAvoidingView,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	useWindowDimensions,
+	View,
+} from "react-native";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -7,6 +17,9 @@ import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 
 function StartGameScreen({ gotoNextStage, enteredNumber, numberInputHandler }) {
+	//for dynamic dimensions we need to use the hook
+	const { width, height } = useWindowDimensions();
+
 	function confirmHandler() {
 		const chosenNumber = parseInt(enteredNumber);
 
@@ -30,37 +43,50 @@ function StartGameScreen({ gotoNextStage, enteredNumber, numberInputHandler }) {
 
 	const isEmpty = enteredNumber === "";
 
+	const marginTop = height < 380 ? 30 : 100;
+
 	return (
-		<View style={styles.rootContainer}>
-			<Title>Guess My Number</Title>
-			<Card>
-				<InstructionText>Enter a Number</InstructionText>
-				<TextInput
-					keyboardType="number-pad"
-					style={styles.numberInput}
-					maxLength={2}
-					onChangeText={numberInputHandler}
-					value={enteredNumber}
-				/>
-				<View style={styles.btnsContainer}>
-					<View style={styles.btnContainer}>
-						<PrimaryButton disabled={isEmpty} onPress={resetText}>
-							Reset
-						</PrimaryButton>
-					</View>
-					<View style={styles.btnContainer}>
-						<PrimaryButton onPress={confirmHandler}>Confirm</PrimaryButton>
-					</View>
+		//with behaviour set as position, the view will move up when keyboard is visible. Also we may need to wrap with ScrollView to support scrolling
+		<ScrollView style={styles.screen}>
+			<KeyboardAvoidingView style={styles.screen} behavior="position">
+				<View style={[styles.rootContainer, { marginTop }]}>
+					<Title>Guess My Number</Title>
+					<Card>
+						<InstructionText>Enter a Number</InstructionText>
+						<TextInput
+							keyboardType="number-pad"
+							style={styles.numberInput}
+							maxLength={2}
+							onChangeText={numberInputHandler}
+							value={enteredNumber}
+						/>
+						<View style={styles.btnsContainer}>
+							<View style={styles.btnContainer}>
+								<PrimaryButton disabled={isEmpty} onPress={resetText}>
+									Reset
+								</PrimaryButton>
+							</View>
+							<View style={styles.btnContainer}>
+								<PrimaryButton onPress={confirmHandler}>Confirm</PrimaryButton>
+							</View>
+						</View>
+					</Card>
 				</View>
-			</Card>
-		</View>
+			</KeyboardAvoidingView>
+		</ScrollView>
 	);
 }
 
+//this will execute only first time. Will not execute on rerenders or orientation changes
+// const deviceHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+	},
 	rootContainer: {
 		flex: 1,
-		marginTop: 100,
+		// marginTop: deviceHeight < 380 ? 30 : 100,
 		alignItems: "center",
 	},
 	btnsContainer: {
