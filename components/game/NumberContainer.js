a3ac4efe90b9dd2ff9a2314dcfada79b4c21bf79
Ananthu S <501618@containerstore.com>
2023-01-03 00:22:08 +0530

    81-dimensions api

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import Colors from "../../constants/colors";
 
 function NumberContainer({ children }) {
@@ -10,19 +10,21 @@ function NumberContainer({ children }) {
 	);
 }
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
 	container: {
 		borderWidth: 4,
 		borderColor: Colors.accent500,
-		padding: 24,
-		margin: 24,
+		padding: deviceWidth < 380 ? 12 : 24,
+		margin: deviceWidth < 380 ? 12 : 24,
 		borderRadius: 8,
 		alignItems: "center",
 		justifyContent: "center",
 	},
 	numberText: {
 		color: Colors.accent500,
-		fontSize: 36,
+		fontSize: deviceWidth < 380 ? 28 : 36,
 		fontFamily: "open-sans-bold",
 	},
 });
